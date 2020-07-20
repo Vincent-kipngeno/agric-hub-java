23d148d920bdc5322f0c60a68d7a1703a2416052
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-20 23:39:33 +0300

    Add properties to Customer class with constructor, getters and setter methods

--- src/main/java/models/Customer.java
+++ src/main/java/models/Customer.java
@@ -1,4 +1,34 @@
 package models;
 
 public class Customer {
+    private int id;
+    private String name;
+    private String location;
+    private String email;
+
+    public Customer(String name, String location, String email) {
+        this.name = name;
+        this.location = location;
+        this.email = email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
