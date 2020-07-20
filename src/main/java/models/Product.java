4489d1d623137cf3aee61fe8b95b3e9550c205f8
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-20 23:42:09 +0300

    Add properties to Product class together with construcor, getter methods and setter methods

--- src/main/java/models/Product.java
+++ src/main/java/models/Product.java
@@ -1,4 +1,22 @@
 package models;
 
 public class Product {
+    private int id;
+    private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
