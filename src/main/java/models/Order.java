0246cccbaf747bf8ca8baf188586529341edc61c
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-20 23:25:49 +0300

    Add properties to Order class with constructor, setter methods and getter methods

--- src/main/java/models/Order.java
+++ src/main/java/models/Order.java
@@ -1,4 +1,45 @@
 package models;
 
 public class Order {
+    private int id;
+    private int customerId;
+    private int productId;
+    private int quantity;
+    private int totalPrice;
+
+    public Order(int id, int customerId, int productId, int quantity, Supply supply) {
+        this.id = id;
+        this.customerId = customerId;
+        this.productId = productId;
+        this.quantity = quantity;
+        setTotalPrice(supply);
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTotalPrice(Supply supply) {
+        this.totalPrice = supply.getPrice() * this.quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
 }
