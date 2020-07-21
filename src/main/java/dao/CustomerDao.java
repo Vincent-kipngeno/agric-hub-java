6c97200e03204b97055ceb29ee3cbd518872b850
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-21 11:07:50 +0300

    Add method signatures to CustomerDao interface for methods to be included in Sql2oCustomerDao class

--- src/main/java/dao/CustomerDao.java
+++ src/main/java/dao/CustomerDao.java
@@ -1,4 +1,21 @@
 package dao;
 
+import models.Customer;
+
+import java.util.List;
+
 public interface CustomerDao {
+    //create
+    void add (Customer customer);
+
+    //read
+    List<Customer> getAll();
+    Customer findById(int id);
+
+    //update
+    void update(int id, String name, String location, String email);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
