5cf0e8ec9730de6afcd915b0c7aaebea5bf45148
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-21 18:31:19 +0300

    Add ProductDao interface with method signatures of methods to be included in Sql2oProductDao

--- src/main/java/dao/ProductDao.java
+++ src/main/java/dao/ProductDao.java
@@ -1,4 +1,21 @@
 package dao;
 
+import models.Product;
+
+import java.util.List;
+
 public interface ProductDao {
+    //create
+    void add (Product product);
+
+    //read
+    List<Product> getAll();
+    Product findById(int id);
+
+    //update
+    void update(int id, String name);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
