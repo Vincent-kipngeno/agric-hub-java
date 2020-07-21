7b83893853e8f16d5a139ba6674b04d5c962f18c
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-07-21 12:19:41 +0300

    Add test methods for testing the behaviours of methods in Sql2oCustomerDao class

--- src/test/java/dao/Sql2oCustomerDaoTest.java
+++ src/test/java/dao/Sql2oCustomerDaoTest.java
@@ -1,8 +1,10 @@
 package dao;
 
+import models.Customer;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
@@ -30,5 +32,74 @@ public class Sql2oCustomerDaoTest {
     public static void shutDown() throws Exception{
         conn.close();
         System.out.println("Connection closed");
+    }
+    @Test
+    public void addingCustomerSetsId() {
+        Customer customer = setCustomer();
+        int originalCustomerId = customer.getId();
+        customerDao.add(customer);
+        assertNotEquals(originalCustomerId, customer.getId());
+    }
+
+    @Test
+    public void add_individualCustomerCanBeFoundById() {
+        Customer customer = setCustomer();
+        customerDao.add(customer);
+        Customer foundCustomer = customerDao.findById(customer.getId());
+        assertEquals(customer, foundCustomer);
+    }
+
+    @Test
+    public void findById_individualCustomerCanBeFoundById() {
+        Customer customer = setCustomer();
+        customerDao.add(customer);
+        Customer foundCustomer = customerDao.findById(customer.getId());
+        assertEquals(customer, foundCustomer);
+    }
+
+    @Test
+    public void getAll_allCustomersAreReturnedCorrectly() {
+        Customer customer = setCustomer();
+        customerDao.add(customer);
+        assertEquals(1, customerDao.getAll().size());
+    }
+
+    @Test
+    public void getAll_nothingIsReturnedFromAnEmptyDatabase() {
+        assertEquals(0, customerDao.getAll().size());
+    }
+
+    @Test
+    public void update_customerIsUpdatedCorrectly() {
+        Customer customer = setCustomer();
+        customerDao.add(customer);
+        int currentId = customer.getId();
+        customerDao.update(currentId, "kevin", "UG", "kipkev@gmail.com");
+        Customer updatedCustomer = customerDao.findById(currentId);
+        assertNotEquals(customer, updatedCustomer);
+    }
+
+    @Test
+    public void deleteById_individualCustomerIsDeletedCorrectlyByItsId() {
+        Customer customer = setCustomer();
+        Customer otherCustomer = setCustomer();
+        customerDao.add(customer);
+        customerDao.add(otherCustomer);
+        customerDao.deleteById(customer.getId());
+        assertEquals(1, customerDao.getAll().size());
+    }
+
+    @Test
+    public void clearAll_allAddedCustomersCanBeCleared() {
+        Customer customer = setCustomer();
+        Customer otherCustomer = setCustomer();
+        customerDao.add(customer);
+        customerDao.add(otherCustomer);
+        customerDao.clearAll();
+        assertEquals(0, customerDao.getAll().size());
+    }
+
+    public Customer setCustomer(){
+        return new Customer("henry", "nike", "hen@gmail.com");
     }
 }
