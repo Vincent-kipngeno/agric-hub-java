54deebbd30672867dea53d9afbcba99578d17dd7
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-07-21 17:49:13 +0300

    Implement FarmerDao

--- src/main/java/dao/FarmerDao.java
+++ src/main/java/dao/FarmerDao.java
@@ -1,4 +1,21 @@
 package dao;
 
+import models.Farmer;
+
+import java.util.List;
+
 public interface FarmerDao {
+    //create
+    void add (Farmer farmer);
+
+    //read
+    List<Farmer> getAll();
+   Farmer getId(int id);
+
+    //update
+    void update(int id, String name, String location, String email);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
