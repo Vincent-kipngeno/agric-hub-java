fa6a873eb9e979876e0aeec10fc7e9ae60695147
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-07-23 13:09:32 +0300

    Fixing my database credentials to make tests run

--- src/test/java/dao/Sql2oFarmerDaoTest.java
+++ src/test/java/dao/Sql2oFarmerDaoTest.java
@@ -23,7 +23,7 @@ public class Sql2oFarmerDaoTest {
     @BeforeClass
     public static void setUp() throws Exception {
         String connectionString = "jdbc:postgresql://localhost:5432/agric_hub_test";
-        Sql2o sql2o = new Sql2o(connectionString, "maureenbett", "Kenyan082bett");
+        Sql2o sql2o = new Sql2o(connectionString, "maureenbett", "kenyan082bett");
         farmerDao = new Sql2oFarmerDao(sql2o);
         productDao = new Sql2oProductDao(sql2o);
         supplyDao = new Sql2oSupplyDao(sql2o);
